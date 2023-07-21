7f6da2075c587b631a25f032f407b2aa7d920d87
Marcus Kim <marcuskim1989@gmail.com>
2023-07-20 20:44:50 -0700

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Marcus Y. Kim on 7/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
